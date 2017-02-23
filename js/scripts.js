d47d3f9fcd70ef9c7a2dde4d0850fb8bd693d058
Chris Finney and Ryan McAlpin <ryan@>
2017-02-23 10:55:34 -0800

    add method to convert input string to an array of reversed numbers representing decimal places, e.g. 541 becomes [1, 40, 500]

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,45 @@
+var decimals = [1, 5, 10, 50, 100, 500, 1000];
+var romans = ["I", "V", "X", "L", "C", "D", "M"];
+var decimalArray = [];
+
 var romanize = function(decimalInput) {
-  var result = "I";
+
+  var stringArray = decimalInput.split("");
+
+  stringToNumber(stringArray);
+  console.log(decimalArray);
+
+  var decimalResults = [];
+  for (var i = 0; i < decimalInput; i++) {
+    decimalResults.push(1);
+  }
+
+
+
+  var result = romans[decimals.indexOf(decimalInput)];
+
   return result;
 }
 
+var stringToNumber = function(array) {
+  array.forEach(function(element){
+    decimalArray.push(parseInt(element));
+  });
+  decimalArray.reverse();
+  var i = 0;
+  decimalArray.forEach(function(element){
+    element = element * (10 ** i);
+    decimalArray.splice(i, 1, element);
+    console.log(decimalArray);
+    i++;
+  });
+}
 
 
 $(function() {
   $("form#form").submit(function(event){
     event.preventDefault();
-    var userInput = parseInt($("#decimal").val());
-    console.log(userInput);
+    var userInput = $("#decimal").val();
     $("#result").text(romanize(userInput));
   });
 });
